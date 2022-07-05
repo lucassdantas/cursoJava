12945380d1b9b9a29aed1d66789a1ae988c90371
Lucas Dantas <95001459+lucassdantas@users.noreply.github.com>
2022-07-05 19:42:58 -0300

    Add files via upload

--- curso/src/curso/CursoProjeto.java
+++ curso/src/curso/CursoProjeto.java
@@ -1,9 +1,20 @@
 package curso;
-import escola.Estudante;
+import calculadora.Calculadora;
+
+import java.util.Locale;
 import java.util.Scanner;
 
 public class CursoProjeto {
 	public static void main (String args[]) {
-
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
+		double raio = scan.nextDouble();
+		double Circunferencia = Calculadora.circunferencia(raio);
+		double volume = Calculadora.volume(raio);
+		double valorPi = Calculadora.PI;
+		System.out.println(Circunferencia);
+		System.out.println(volume);
+		System.out.println(valorPi);
+		scan.close();
 	}
 }
